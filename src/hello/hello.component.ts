74db70260b9d071c41417714958f1a5161024dfb
DaniiTonello <daniela.a.tonello@gmail.com>
2022-11-22 14:28:22 -0300

    third branch

--- src/hello/hello.component.ts
+++ src/hello/hello.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'custom-hello',
-    templateUrl: './hello.component.html'
+    templateUrl: './hello.component.html',
+    styleUrls: ['./hello.component.css'] //styleUrls puede aceptar varias hojas de estilo a la vez, por eso está dentro de un array
 })
 
 export class HelloComponent{}
