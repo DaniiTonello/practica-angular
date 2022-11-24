604cc0c73e2297467cd624edf8a1baf6b34425e1
DaniiTonello <daniela.a.tonello@gmail.com>
2022-11-23 21:41:07 -0300

    nineth branch

--- src/hello/hello.component.ts
+++ src/hello/hello.component.ts
@@ -10,10 +10,24 @@ export class HelloComponent{
     @Input() name?:string; //pasa un valor de fuera del componente(anterior) hacia dentro del componente gracias al valor Input para que pueda ser REUTILIZABLE
     @Output() sayHello:EventEmitter<string> = new EventEmitter<string>(); //solo emitira eventos de tipo string
 
+    public nameList:Array<string> = [];
+    public inputName:string = 'John';
+
     constructor(){
     }
 
     onNameClick() {
         this.sayHello.emit('Hello!');
+    }
+
+    addName(){
+        this.nameList.push(this.inputName);
+        this.inputName = '';
+        console.log(this.nameList);
+    }
+
+    updateInputName(event:any){
+        this.inputName = event.target.value;
+        console.log(this.inputName);
     }
 }
